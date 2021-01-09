349a7a2f95e79b0088cb7000d114165589226e06
Lukasz <gorcek45@gmail.com>
2021-01-09 22:02:11 +0100

    teamservice plan

--- src/main/java/pl/lukaszg/sportapp/controller/TeamController.java
+++ src/main/java/pl/lukaszg/sportapp/controller/TeamController.java
@@ -17,4 +17,10 @@ public class TeamController {
     public Team findTeamById(Long id) {
         return teamService.findTeamById(id);
     }
+
+    //lista teamow z danymi
+    // zawodnicy
+    //ostatnie mecze z page 3
+    // ostatnie turnieje z page 3
+    // trofea
 }
