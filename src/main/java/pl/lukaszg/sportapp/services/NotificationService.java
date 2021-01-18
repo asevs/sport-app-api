355ca485985c92bb559fa631b69d348784d0f6b7
Lukasz <gorcek45@gmail.com>
2021-01-18 19:45:33 +0100

    Email sender

--- src/main/java/pl/lukaszg/sportapp/services/NotificationService.java
+++ src/main/java/pl/lukaszg/sportapp/services/NotificationService.java
@@ -5,15 +5,19 @@ import pl.lukaszg.sportapp.model.Notification;
 import pl.lukaszg.sportapp.model.NotificationType;
 import pl.lukaszg.sportapp.model.User;
 
+import javax.mail.MessagingException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service("notificationService")
 public class NotificationService {
     private final UserService userService;
+    private final MailService mailService;
 
-    public NotificationService(UserService userService) {
+    public NotificationService(UserService userService, MailService mailService) {
+
         this.userService = userService;
+        this.mailService = mailService;
     }
 
     public void createNotification(Long userId, NotificationType type) {
@@ -23,7 +27,14 @@ public class NotificationService {
         notification.setType(type);
         notification.setCreatedDate(LocalDateTime.now());
         user.setNotifications(notifications);
-        userService.changeProfile(user.getId(),user);
+        userService.changeProfile(user.getId(), user);
+        if (user.isSendMail()) {
+            try {
+                mailService.sendMail(user.getEmail(), type.toString(), "New" + type.toString(), false);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
